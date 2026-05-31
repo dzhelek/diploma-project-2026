9d96cce37211efa090dd416ed37a1845d330db7f
Yoan Dzhelekarski <yoan.dzhelekarski@gmail.com>
2026-05-31 17:30:27 +0300

    feat: add ascon

--- Slave/src/main.cpp
+++ Slave/src/main.cpp
@@ -1,11 +1,41 @@
 #include <Arduino.h>
 
+#include "crypto_aead.h"
+// #include "yoan.h"
+
 // put function declarations here:
 int myFunction(int, int);
+
+// Ascon-AEAD128 example
+  unsigned char n[32] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+                         11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                         22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+  unsigned char k[32] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+                         11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                         22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+  unsigned char a[32] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+                         11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                         22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+  unsigned char m[32] = {0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+                         11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                         22, 23, 24, 25, 26, 27, 28, 29, 30, 31};
+  unsigned char c[32], h[32], t[32];
+  unsigned long long alen = 16;
+  unsigned long long mlen = 16;
+  unsigned long long clen;
+  int result = 0;
 
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+
+    printf("input:\n");
+  result |= crypto_aead_encrypt(c, &clen, m, mlen, a, alen, (unsigned char *)0, n, k);
+  printf("encrypt:\n");
+  result |= crypto_aead_decrypt(m, &mlen, (unsigned char *)0, c, clen, a, alen, n, k);
+  printf("decrypt:\n");
+  // yoan();
+
 }
 
 void loop() {
