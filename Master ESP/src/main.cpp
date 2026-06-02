6c79ad169cfec9032f511252ca57530c540ea2fd
Yoan Dzhelekarski <yoan.dzhelekarski@gmail.com>
2026-06-02 14:02:20 +0300

    feat: connect to mqtt and send test data

--- Master ESP/src/main.cpp
+++ Master ESP/src/main.cpp
@@ -1,18 +1,72 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#include "secrets.h"
 
 // put function declarations here:
 int myFunction(int, int);
+void setup_wifi();
+void reconnect();
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  client.setCallback([](char* topic, byte* payload, unsigned int length) {
+    Serial.print("Message arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+  });
+  setup_wifi();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (!client.connected()) reconnect();
+  client.loop();
+
+  // Публикуване на данни на всеки 5 секунди
+  static unsigned long lastMsg = 0;
+  if (millis() - lastMsg > 5000) {
+    lastMsg = millis();
+    const char* msg = "hello";
+    client.publish("esp32/temperature", msg);
+  }
 }
 
 // put function definitions here:
 int myFunction(int x, int y) {
   return x + y;
+}
+
+void setup_wifi() {
+  delay(10);
+  Serial.print("Connecting to ");
+  Serial.println(WIFI_SSID);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nWiFi connected");
+  Serial.println("IP: " + WiFi.localIP().toString());
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect("ESP32Client")) {  // Уникален Client ID
+      Serial.println("connected");
+      client.subscribe("esp32/output");   // subscription
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5s");
+      delay(5000);
+    }
+  }
 }
